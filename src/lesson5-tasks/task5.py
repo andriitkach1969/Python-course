addbdf7f86feb075bac781fd24d177a65f9c0b9f
Andrii Tkach <papa.tkach@gmail.com>
2020-03-07 19:40:00 +0200

    task 5 solved

---
+++ src/lesson5-tasks/task5.py
@@ -0,0 +1,15 @@
+import random
+
+dimSize = 20
+xBound = 1
+yBound = 11
+dim = [random.randint(xBound, yBound) for i in range(dimSize+1)]
+
+checkNumberStr = input('Please enter the number in range from {0} to {1} to check: '.format(xBound, yBound))
+try:
+    checkNumber = int(checkNumberStr)
+    checkResult = dim.count(checkNumber)
+    print('Array generated: \n', dim)
+    print('Number {0} is repeated in array {1} time(s)'.format(checkNumber, checkResult))
+except Exception as e:
+    print(e)
