942e70a25cfcd48d22d9aa486b515ccf8d0d1131
Andrii Tkach <papa.tkach@gmail.com>
2020-03-11 10:34:18 +0200

    task 1 task2 task3 completed

--- src/lesson6_tasks/task2.py
+++ src/lesson6_tasks/task2.py
@@ -3,8 +3,9 @@ import src.lesson6_tasks.globals as g
 minLen = 100
 
 for word in g.text.split():
-    if minLen > len(word):
-        minLen = len(word)
+    _tmp = len(word)
+    if minLen > _tmp:
+        minLen = _tmp
 print('Minimum word\'s length is {0}.'.format(minLen))
 
 
--- src/lesson6_tasks/task3.py
+++ src/lesson6_tasks/task3.py
@@ -8,16 +8,12 @@ for word in g.text.split():
     else:
         ratings[word[0].lower()] = ratings.get(word[0].lower()) + 1
 print(ratings)
-for i in ratings:
-    print(i)
 
 maxRateKey = ''
 maxRate = 0
 for i in ratings:
     _tmp = ratings.get(i)
-    print(_tmp)
     if maxRate < _tmp:
         maxRateKey = i
         maxRate = _tmp
-        print(i)
-print('The most words in text start with "{0}" char, amount {1}'.format(maxRateKey, ratings.get(maxRateKey)))
+print('The most words in text ({1} items) start with "{0}" char'.format(maxRateKey, ratings.get(maxRateKey)))
